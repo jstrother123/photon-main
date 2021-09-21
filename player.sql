596a9040d337f88228415c0bbfe0f127f202a3f3
jstrother123 <jim.p.strother@gmail.com>
2021-09-21 13:38:51 -0500

    Add files via upload

---
+++ player.sql
@@ -0,0 +1,19 @@
+
+-- Create database
+CREATE DATABASE game;
+
+--Connect to database
+\c game
+
+--Create table
+CREATE TABLE player (
+  id INT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  codename VARCHAR(30)
+);
+
+--Place first record into table
+INSERT INTO player (id, first_name, last_name, codename)
+VALUES (1, 'Jim', 'Strother', 'Opus');
+
