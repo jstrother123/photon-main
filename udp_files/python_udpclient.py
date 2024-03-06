40997b5f1ab1f3197006e7e4576756b2aa636a86
jstrother123 <jim.p.strother@gmail.com>
2024-03-06 15:10:43 -0600

    Update python_udpclient.py

--- udp_files/python_udpclient.py
+++ udp_files/python_udpclient.py
@@ -2,7 +2,7 @@ import socket
 
 msgFromClient       = "Hello UDP Server"
 bytesToSend         = str.encode(msgFromClient)
-serverAddressPort   = ("127.0.0.1", 20001)
+serverAddressPort   = ("127.0.0.1", 7501)
 bufferSize          = 1024
 
 # Create a UDP socket at client side
