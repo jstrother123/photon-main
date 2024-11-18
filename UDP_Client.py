2a24438ecabb1a8ad729335191f05c6aab83a424
yyyyyyyyyi099 <yiwang0625@gmail.com>
2024-11-18 00:29:07 -0600

    Update UDP_Client.py
    Adding player's team

--- UDP_Client.py
+++ UDP_Client.py
@@ -2,39 +2,41 @@ import socket
 import json
 
 # Call the method and pass in the info that user entered
-def passInfo(id, name, equipNum):
+def passInfo(id, name, equipNum, team):
     try:
         # Greeting
         msgFromClient1 = "Hello UDP Sever, here's the user data"
         bytesToSend1 = str.encode(msgFromClient1)
 
         # Player's info
-        #msgFromClient2 = f"{id},{name},{equipNum}"
-        msgFromClient2 = json.dumps({"id": id, "name": name, "equipNum": equipNum})
+        msgFromClient2      = f"{id},{name},{equipNum},{team}"
+        msgFromClient2      = json.dumps({"id": id, "name": name, "equipNum": equipNum, "team": team})
         bytesToSend2        = str.encode(msgFromClient2)
+        #print(msgFromClient2)
+        #print("Receive from main")
 
         # Server's info
-        serverAddressPort   = ("127.0.0.1", 7501)
+        serverAddressPort   = ("127.0.0.1", 7500)
         bufferSize          = 1024
 
         # Create a UDP socket at client side
         UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
-        # Send the greeting msg to server using created UDP socket
+        # Send the msg to server using created UDP socket
         UDPClientSocket.sendto(bytesToSend1, serverAddressPort)
-        # Receive the response
+        
+        # Receiving
         msgFromServer = UDPClientSocket.recvfrom(bufferSize)
         msg = "Message from Server {}".format(msgFromServer[0])
-
-        print(msg)
+        #print(msg)
 
         # Send the user's info
         UDPClientSocket.sendto(bytesToSend2, serverAddressPort)
+        #print("Successully send.")
 
         msgFromServer = UDPClientSocket.recvfrom(bufferSize)
         msg = "Message from Server {}".format(msgFromServer[0])
-
-        print(msg)
+        #print(msg)
 
     except socket.error as e:
         print(f"Socket error: {e}")
