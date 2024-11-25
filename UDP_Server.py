b86272b75100f2a70c96fae073b7f31466f0826b
yyyyyyyyyi099 <yiwang0625@gmail.com>
2024-11-25 13:37:23 -0600

    Update UDP_Server.py

--- UDP_Server.py
+++ UDP_Server.py
@@ -1,79 +1,65 @@
 import socket
 import json
 import os
-#import GameActionScreen
-#from GameActionScreen import
+import UDP_Client
+from UDP_Client import passInfo
+import GameActionScreen
+from GameActionScreen import scoring
+from GameActionScreen import greenGotBase
+from GameActionScreen import redGotBase
 
-localIP = "127.0.0.1"
-localPort = 7501
-clientPort = 7500
+
+serverIP = "127.0.0.1"
+serverPort = 7501
 bufferSize = 1024
-msgFromServer = "Hello UDP Client, data has been received"
-bytesToSend = str.encode(msgFromServer)
+clientPort = 7500
 
 # Create a datagram socket
 UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
 
 # Bind to address and ip
-UDPServerSocket.bind((localIP, clientPort))
+UDPServerSocket.bind((serverIP, serverPort))
 
 print("UDP server up and listening")
-
-# Json file path
-file_path = 'players_score.json'
-
-# Check if the file exist or not, if not, create the file
-if not os.path.exists(file_path):
-    players_data = {}
-    with open(file_path, 'w') as file:
-        json.dump(players_data, file)
 
 # Listen for incoming datagrams
 while (True):
     bytesAddressPair = UDPServerSocket.recvfrom(bufferSize)
     message = bytesAddressPair[0]
     address = bytesAddressPair[1]
+    playerId = message.decode('utf-8')
     clientMsg = "Message from Client:{}".format(message)
     clientIP = "Client IP Address:{}".format(address)
-
+    
+    #print(message)
     #print(clientMsg)
     #print(clientIP)
-
-    # Load the file already exist
-    with open(file_path, 'r') as file:
-        players_data = json.load(file)
-
-    try:
-        decoded_message = message.decode('utf-8')
-        player_info = json.loads(decoded_message)
-
-        player_id = player_info.get("id")
-        player_name = player_info.get("name")
-        player_equipNum = player_info.get("equipNum")
-        player_team = player_info.get("team")
-        #print(player_id, player_name, player_equipNum, player_team)
-
-        # Insert or update the user's data
-        players_data[player_name] = {
-            "id": player_id,
-            "equipNum": player_equipNum,
-            "team" : player_team
-        }
-
-        # Save the data after updated
-        with open(file_path, 'w') as file:
-            json.dump(players_data, file, indent=4)
-
-        #print(f"Updated player data: {players_data}")
-
-    except json.JSONDecodeError:
-        print("Failed to decode JSON from client")
-
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-    # Sending a reply to client
-    UDPServerSocket.sendto(bytesToSend, address)
-
+    
+    # Once the game starts
+    try: 
+        id1, id2 = map(int, playerId.split(":"))
+        #id1, id2 = clientMsg.split(":")
+        #print(id1, id2)
+			
+        # Green hitting base
+        if (id2 == 53):
+            greenGotBase(id1)
+            #print("Green just hit the base!")
+            passInfo(id1)
+            
+        # Red hitting base
+        elif (id2 == 43):
+            redGotBase(id1)
+            #print("Red just hit the base!")
+            passInfo(id1)
+            
+        # Scoring, id2 got hit
+        else:
+            scoring(id1, id2)
+            passInfo(id2)
+            
+    except ValueError:
+        print("Invalid message format, skipping...")
+    
 # Close the server socket
 UDPServerSocket.close()
