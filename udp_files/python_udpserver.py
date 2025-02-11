817830927270f11edc63c4d8445744abbad703de
jstrother123 <jim.p.strother@gmail.com>
2025-02-11 10:25:08 -0600

    Update python_udpserver.py

--- udp_files/python_udpserver.py
+++ udp_files/python_udpserver.py
@@ -1,7 +1,7 @@
 import socket
 
 localIP     = "127.0.0.1"
-localPort   = 20001
+localPort   = 7501
 bufferSize  = 1024
 msgFromServer       = "Hello UDP Client"
 bytesToSend         = str.encode(msgFromServer)
